cda998e6d4b1c3c9e933f402571a864240ffa9b3
BouvetMP <moralesprietojuandiego@gmail.com>
2026-06-25 15:15:51 -0500

    Delete FrontEnd directory

--- FrontEnd/trida-api/src/routes/banks.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { listBanks } from '../controllers/banks.controller.js';
-
-const router = Router();
-
-router.get('/', listBanks);
-
-export default router;
--- FrontEnd/trida-api/src/routes/devices.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getDevice, listDevices } from '../controllers/devices.controller.js';
-
-const router = Router();
-
-router.get('/', listDevices);
-router.get('/:id', getDevice);
-
-export default router;
